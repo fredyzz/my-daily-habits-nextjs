ddb96d77667a3ebc28bbc3aeff376eb668a1adea
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2024-01-04 18:24:09 +0100

    Create seed function to add users to db

---
+++ mocks/placeholder-data.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442a',
+    name: 'Fredy',
+    username: '123456',
+    email: 'fredy@zziotdashboard.com'
+  }
+];
+
+module.exports = {
+  users
+};
